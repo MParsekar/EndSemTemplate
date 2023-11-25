4932c8d78db3c7838a3d6de6722a734c4c7f0d7b
mahadevv-at-719945103823 <mahadevv-at-719945103823>
2023-11-25 09:10:02 +0530

    first

---
+++ src/test/java/mes/finalexam/labwork/LabworkApplicationTests.java
@@ -0,0 +1,13 @@
+package mes.finalexam.labwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
